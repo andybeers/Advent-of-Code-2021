1bdd0ddac349af7169b9f524f60eccc1a3a84fd7
Andy Beers <andy@koan.co>
2021-12-07 18:51:41 -0500

    Implement day 1 part 1 solution

--- 01/index.ts
+++ 01/index.ts
@@ -1,3 +1,17 @@
 import { getNumbers } from "../utils";
 
 const depths = getNumbers();
+
+function getDepthIncreaseCount(depths: number[]): number {
+  let depthIncreaseCount = 0;
+
+  for (let i = 1; i < depths.length; i++) {
+    if (depths[i] > depths[i - 1]) {
+      depthIncreaseCount++;
+    }
+  }
+
+  return depthIncreaseCount;
+}
+
+console.log(getDepthIncreaseCount(depths));
