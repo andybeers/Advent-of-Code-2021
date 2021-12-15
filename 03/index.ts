f6c2ce6a0fc4f4714073e5cd02b96501d9cb40b6
Andy Beers <andy@koan.co>
2021-12-14 20:37:59 -0500

    Solve part 2; add tsconfig

--- 03/index.ts
+++ 03/index.ts
@@ -42,3 +42,104 @@ console.log(
   "Power Consumption: ",
   convertToDecimal(gammaRate) * convertToDecimal(epsilonRate)
 );
+
+function getMostCommonValue({
+  values,
+  tiebreakerValue,
+}: {
+  values: string[];
+  tiebreakerValue: string;
+}): string {
+  let ones = 0;
+  let zeros = 0;
+
+  values.forEach((value) => {
+    if (value === "1") {
+      ones++;
+    } else {
+      zeros++;
+    }
+  });
+
+  if (ones === zeros) {
+    return tiebreakerValue;
+  }
+
+  if (ones > zeros) {
+    return "1";
+  }
+
+  return "0";
+}
+
+function getLeastCommonValue({
+  values,
+  tiebreakerValue,
+}: {
+  values: string[];
+  tiebreakerValue: string;
+}): string {
+  let ones = 0;
+  let zeros = 0;
+
+  values.forEach((value) => {
+    if (value === "1") {
+      ones++;
+    } else {
+      zeros++;
+    }
+  });
+
+  if (ones === zeros) {
+    return tiebreakerValue;
+  }
+
+  if (ones > zeros) {
+    return "0";
+  }
+
+  return "1";
+}
+
+function getOxygenScrubberRating() {
+  let oxygenScrubberRatingValues = binaryInput;
+
+  for (let i = 0; i < binaryInput[0].length; i++) {
+    const positionValues = oxygenScrubberRatingValues.map((value) => value[i]);
+    oxygenScrubberRatingValues = oxygenScrubberRatingValues.filter(
+      (value) =>
+        value[i] ===
+        getMostCommonValue({ values: positionValues, tiebreakerValue: "1" })
+    );
+
+    if (oxygenScrubberRatingValues.length === 1) {
+      return oxygenScrubberRatingValues[0];
+    }
+  }
+}
+
+function getCO2ScrubberRating() {
+  let co2RatingValues = binaryInput;
+
+  for (let i = 0; i < binaryInput[0].length; i++) {
+    const positionValues = co2RatingValues.map((value) => value[i]);
+    co2RatingValues = co2RatingValues.filter(
+      (value) =>
+        value[i] ===
+        getLeastCommonValue({ values: positionValues, tiebreakerValue: "0" })
+    );
+
+    if (co2RatingValues.length === 1) {
+      return co2RatingValues[0];
+    }
+  }
+}
+
+function deriveLifeSupportRating() {
+  const oxygenScrubbberRating = convertToDecimal(getOxygenScrubberRating());
+  const co2ScrubberRating = convertToDecimal(getCO2ScrubberRating());
+
+  return oxygenScrubbberRating * co2ScrubberRating;
+}
+
+console.log("Life Support Rating: ", deriveLifeSupportRating());
